1ed5d946852ebcc949ca563e1cd41559c36ff0ea
Lifirenko Bohdan <qp123456ex@gmail.com>
2025-08-25 12:09:35 +0300

    change junit5 to junit4 dependency.

--- src/test/java/com/gradlehero/themepark/RideStatusServiceTest.java
+++ src/test/java/com/gradlehero/themepark/RideStatusServiceTest.java
@@ -1,25 +1,15 @@
 package com.gradlehero.themepark;
 
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.Assert.assertNotNull;
 
 public class RideStatusServiceTest {
 
-    @ParameterizedTest(name = "{index} gets {0} ride status")
-    @ValueSource(strings = {"rollercoaster", "kogfume", "teacups"})
-    public void getsRideStatus(String ride) {
-        String rideStatus = RideStatusService.getRideStatus(ride);
-        assertNotNull(rideStatus);
-    }
-
     @Test
     public void unknowRideCausesFailure() {
-        assertThrows(IllegalArgumentException.class, () -> {
-            RideStatusService.getRideStatus("dodgems");
-        });
+        String rideStatus = RideStatusService.getRideStatus("teacups");
+
+        assertNotNull(rideStatus);
     }
 }
