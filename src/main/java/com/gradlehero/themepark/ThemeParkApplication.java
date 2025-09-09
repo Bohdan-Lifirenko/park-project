eb8650acef6291713a6d6ebeba178832de909838
Lifirenko Bohdan <qp123456ex@gmail.com>
2025-09-09 14:48:41 +0300

    Exclude transitive logging dependency, to use another logging library.

--- src/main/java/com/gradlehero/themepark/ThemeParkApplication.java
+++ src/main/java/com/gradlehero/themepark/ThemeParkApplication.java
@@ -3,10 +3,9 @@ package com.gradlehero.themepark;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ThemeParkApplication extends SpringBootServletInitializer {
+public class ThemeParkApplication {
     public static void main(String[] args) {
         SpringApplication.run(ThemeParkApplication.class);
     }
