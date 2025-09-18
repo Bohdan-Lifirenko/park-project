c58fd2c790eaf196c3b8f06d4edbdbf67dc1677c
Bohdan <qp123456ex@gmail.com>
2025-09-18 14:08:41 +0300

    Put up our old projects:  app:standard spring web app  service: lib with service class

--- app/src/main/java/com/gradlehero/App.java
+++
@@ -1,9 +0,0 @@
-package com.gradlehero;
-
-import com.gradlehero.themepark.MessageService;
-
-public class App {
-    public static void main(String[] args) {
-        System.out.println(MessageService.generateMessage());
-    }
-}
---
+++ service/src/test/java/com/gradlehero/themepark/RideStatusServiceTest.java
@@ -0,0 +1,30 @@
+package com.gradlehero.themepark;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class RideStatusServiceTest {
+
+//    @ParameterizedTest(name = "{index} gets {0} ride status")
+//    @ValueSource(strings = {"rollercoaster", "kogfume", "teacups"})
+//    public void getsRideStatus(String ride) {
+//        String rideStatus = RideStatusService.getRideStatus(ride);
+//        assertNotNull(rideStatus);
+//    }
+//
+//    @Test
+//    public void unknowRideCausesFailure() {
+//        assertThrows(IllegalArgumentException.class, () -> {
+//            RideStatusService.getRideStatus("dodgems");
+//        });
+//    }
+
+    @Test
+    public void print() {
+        System.out.println(RideStatusService.getRideStatus("kogfume"));
+    }
+}
